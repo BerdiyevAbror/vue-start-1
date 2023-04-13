48b64a46b99896a9fe4283bb9ac8aef07ed88884
BerdiyevAbror <islamiskiyaxi@gmail.com>
2023-04-13 06:25:43 +0500

    tailwin

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+import './tailwind.css'
 import store from "./Store";
+
 
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
--- src/views/40.vue
+++ src/views/40.vue
@@ -1,5 +1,16 @@
 <template>
     <div>
-        404
+        <img class="bgimage" :src="bgImage" alt="">
     </div>
 </template>
+
+<script>
+export default {
+    data(){
+        return{
+        bgImage: "src/assets/404-error-with-landscape-concept-illustration_114360-7898.avif"
+        }
+    }
+}
+</script>
+
